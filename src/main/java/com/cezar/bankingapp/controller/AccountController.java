7f9b4d30df508587cca6c6396915868c0a119892
cezar04 <iancu.m.cezar@gmail.com>
2021-10-24 15:41:09 +0300

    account controller=>add account

---
+++ src/main/java/com/cezar/bankingapp/controller/AccountController.java
@@ -0,0 +1,27 @@
+package com.cezar.bankingapp.controller;
+
+import com.cezar.bankingapp.account.AccountDAO;
+import com.cezar.bankingapp.account.service.AccountService;
+import com.cezar.bankingapp.transaction.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+
+    private AccountService accountService;
+    private TransactionService transactionService;
+
+    @Autowired
+    public AccountController(AccountService accountService, TransactionService transactionService) {
+        this.accountService = accountService;
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping("/add-account/{customerNumber}")
+    public ResponseEntity<?> addNewAccount(@RequestBody AccountDAO accountDAO, @PathVariable Long customerNumber){
+        return accountService.addNewAccount(accountDAO,customerNumber);
+    }
+}
